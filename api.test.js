271b583b724273e57b8e87731aeb78f82811e9f5
FrancoRosa <km115.franco@gmail.com>
2025-07-28 13:02:17 -0500

    standarize logs

--- api.test.js
+++ api.test.js
@@ -7,14 +7,10 @@ const payload2 = ">print"
 
 const payload3 = `
 
-
 PROMPTS- 12:****  
 
      MIDNIGHT TOTALS FOR: JUL 27, 2025
 QUANTITIES FOR PCT 1:
-
-
-
 
 
 POS 1: 1  POS 2: 2  POS 3: 3  POS 4: 4  POS 5: 5  POS 6: 6  POS 7: 9  POS 8: 10
@@ -23,26 +19,15 @@ POS 1: 1  POS 2: 2  POS 3: 3  POS 4: 4  POS 5: 5  POS 6: 6  POS 7: 9  POS 8: 10
 QUANTITIES FOR PCT 2:
 
 
-
-
-
-
 POS 1: 8  POS 2: X  POS 3: X  POS 4: X  POS 5: X  POS 6: X  POS 7: X  POS 8: X 
 --------- --------- --------- --------- --------- --------- --------- ---------
     0.000     0.000     0.000     0.000     0.000     0.000     0.000     0.000
 QUANTITIES FOR PCT 3:
 
 
-
-
-
-
 POS 1: 11 POS 2: 12 POS 3: 13 POS 4: X  POS 5: X  POS 6: X  POS 7: X  POS 8: X 
 --------- --------- --------- --------- --------- --------- --------- ---------
     0.000     0.000     0.000     0.000     0.000     0.000     0.000     0.000
-
-
-
 
 
     PRODUCT QUANTITIES
